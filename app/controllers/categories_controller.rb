574f0934f3135b7aff79dd5c723faa5c74cd8789
Ximena Calderón <mxcalderons@gmail.com>
2022-01-21 13:43:57 -0500

    Show/destroy category photos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,9 +18,19 @@ class CategoriesController < ApplicationController
   end
   
   # Ximena
+  # GET /categories/:id
   def show
+    @category = Category.find(params[:id])
+    @photos = @category.photo
+    @comments = @category.comment
+    # render category/show
   end
 
+  # DELETE /categories/:id
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path, status: :see_other
   end
 end
