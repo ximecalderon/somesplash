fc04c54a49171f5cf2409a288a43ed155a115d99
Alonso <alonsozf.94@gmail.com>
2022-01-22 03:00:50 -0500

    Error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,15 @@ class CommentsController < ApplicationController
   # POST /photos/:photo_id/comments
   def create
     @commentable_object =  params[:category_id] ? Category.find_by_id(params[:category_id]) : Photo.find_by_id(params[:photo_id])
-    @comment = @commentable_object.comments.new(comment_params)
+    @comment = @commentable_object.comments.create(comment_params)
     
     if @comment.save
       redirect_to category_path(@commentable_object) if @commentable_object.instance_of? Category
       redirect_to photo_path(@commentable_object) if @commentable_object.instance_of? Photo
     else
-      puts @comment.errors.full_messages
-      redirect_to category_path(@commentable_object) if  @commentable_object.instance_of? Category
-      redirect_to photo_path(@commentable_object) if @commentable_object.instance_of? Photo
+      redirect_to category_path(@commentable_object) if @commentable_object.instance_of? Category
+      #redirect_to photo_path(@commentable_object) if @commentable_object.instance_of? Photo
+      redirect_to photo_path(@commentable_object), id: params[:photo_id] if @commentable_object.instance_of? Photo
     end
   end
 
