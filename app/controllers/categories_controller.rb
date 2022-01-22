c11ef22ad6cfc28915de14cdeaa131f32c7477ab
jesustr20 <jesus.tr61@gmail.com>
2022-01-21 21:49:25 -0500

    commiteado 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,9 +5,16 @@ class CategoriesController < ApplicationController
 
   # Jesus
   def new
+    @categories = Category.new
   end
 
   def create
+    @categories = Category.new(category_params)
+    if @categories.save
+      redirect_to categories_path
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   # Claudia
@@ -67,5 +74,10 @@ class CategoriesController < ApplicationController
     @category.destroy
 
     redirect_to categories_path, status: :see_other
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :description, :cover)
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,8 +1,8 @@
 module CategoriesHelper
     def show_errors(record, attribute)
-        errors = record.errors.full_messages_for(attribute).map do |message|
-          content_tag(:span, message,class: "input__error-message") #class: "input__error-message"
-        end
-        safe_join(errors)
+      errors = record.errors.full_messages_for(attribute).map do |message|
+        content_tag(:span, message,class: "input__error-message") #class: "input__error-message"
       end
+      safe_join(errors)
+    end
 end
